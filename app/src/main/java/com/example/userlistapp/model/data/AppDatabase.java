846d65c8a6a0a0205cb1cfa70599ea6c9dd03669
shroog <shroog.1997s@gmail.com>
2024-08-04 14:41:46 +0300

    Updated  DataBase Version

--- app/src/main/java/com/example/userlistapp/model/data/AppDatabase.java
+++ app/src/main/java/com/example/userlistapp/model/data/AppDatabase.java
@@ -2,7 +2,7 @@ package com.example.userlistapp.model.data;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {UserEntity.class}, version =2)
+@Database(entities = {UserEntity.class}, version =1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
 }
